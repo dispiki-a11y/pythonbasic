6faa165e0821e40c5ac370ae265edb3554c0bd7c
dispiki-a11y <ocoyoacan19@gmail.com>
2026-03-02 16:00:06 -0800

    cambio en el archivo enemigo.py main.py ogro .py zombie.py con la descripcion de pythonbasic

--- enemigo.py
+++ enemigo.py
@@ -1,21 +1,24 @@
-class Enemigo:
+class enemigo:
     tipo_enemigo: str
     puntos_energia: int = 10
     ataque = 1
 
-    def _init_(self, tipo_enemigo, puntos_energia=10, ataque=1):
-        self._tipo_enemigo = tipo_enemigo
+    def __init__(self, tipo_enemigo, puntos_energia=10, ataque=1):
+        self.__tipo_enemigo = tipo_enemigo
         self.puntos_energia = puntos_energia
         self.ataque = ataque
 
     def get_tipo_enemigo(self):
-        return self._tipo_enemigo
-
+        return self.__tipo_enemigo
+    
     def habla(self):
-        print(f"yo soy {self._tipo_enemigo}. preparado para pelear!!!")
+        print(f"yo soy {self.__tipo_enemigo}. Preparado para pelear!!")
 
-    def camina(self):
-        print(f"{self._tipo_enemigo} se mueve cerca de ti!!!")
+    def caminar(self):
+        print(f"{self.__tipo_enemigo} Se mueve cerca de ti!!!")
 
     def atacar(self):
-        print(f"{self._tipo_enemigo} ataca con un {self.ataque} ")
+        print(f"{self.__tipo_enemigo} ataca con un {self.ataque} de daño !!")
+
+    def ataque_especial(self):
+        print("Enemigo no tiene ataque especial")
--- zombie.py
+++ zombie.py
@@ -1,6 +1,7 @@
 from enemigo import *
+import random
 
-class Zombie(Enemigo):
+class Zombie(enemigo):
     def __init__(self, puntos_energia=10, ataque=1):
         super().__init__(tipo_enemigo="Zombie", puntos_energia=puntos_energia, ataque=ataque)
      
